575f0580cd9f937ea028c9d1ae12d4280dd18efb
Rafael Herrera <santoElias65537@gmail.com>
2020-09-17 01:35:00 -0600

    comment goes here

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Contact Us"/>
   </div>
 </template>
 
@@ -26,3 +25,18 @@ export default {
   margin-top: 60px;
 }
 </style>
+
+<!-- 
+what it needs 
+1. user name - cannot be blank
+2. email - cannot be nlank and must be a valid email address
+3. birth date - can be blank
+             if not empty, the input must be a valid date
+4. check box - afreement ot be contacted - MUST BE CHECKKED
+
+5. submit button is disabl;e if anay of the previous fields are invalid
+6. form has a clear button that clears the rest of the of the form
+7. to submit he from, make an http post request to 
+ const httpPostRequest = https://my-json-server.typicode.com/JustUtahCoders/interview-users-api/users 
+-->
+
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,107 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <input v-model="name" placeholder="Enter Name" />
+    <br />
+    <input v-model="email" placeholder="Enter valid email" />
+    <br />
+    <input v-model="birthDate" placeholder="Enter birth date" />
+    <br />
+    <button v-on:click="main">Submit</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+    // name: String,
+    // email: String,
+    // birthDate: String
+  },
+  data: function () {
+    return {
+      name: "",
+      email: "",
+      birthDate: "",
+      isValid: {
+        name: false,
+        email: false
+        // birthDate: false,
+      },
+      isNameValid: false,
+      isEmailValid: false
+    };
+  },
+  methods: {
+    main: function () {
+      // console.log(this.changeValidStatus(this.isValidName, this.name))
+      // const validName = this.changeValidStatus(this.isValidName, this.name)
+      // this.valid.name = validName
+      // this.isNameValid = true;
+      // this.isEmailValid = true;
+
+      this.isNameValid = this.changeValidStatus(this.isValidName, this.name)
+
+      this.isEmailValid = this.changeValidStatus(this.isValidEmail, this.email)
+
+      if(this.isEverythingValid()) {
+        alert("shit worked")
+      } else {
+        alert("please fill everything before submitting")
+      }
+
+
+    
+    },
+    test: function() {
+      console.log(this.isEverythingValid())
+    },
+    isEverythingValid: function() {
+      const validInfo = [this.isNameValid, this.isEmailValid];
+
+      for(let i = 0; i < validInfo.length; i++) {
+        if(validInfo[i] == false) {
+          return false
+        }
+      }
+
+      return true;
+
+      // console.log(validInfo.some((e) => {e = true;}))
+
+      // bject.keys(isValidInfo).forEach(function (key) {
+      //   console.log(key, isValidInfo[key]);
+      // }) 
+
+
+
+      // const isValidInfo = this.isValid
+
+      // Object.keys(isValidInfo).forEach(function (key) {
+      //   console.log(key, isValidInfo[key]);
+
+      // }) 
+    },
+    changeValidStatus: function(test, element) {
+      return test(element);
+    },
+    isValidName: function (name) {
+      //returns true if string is filled, makes it so string filled with empty space returns false
+      return /\S/.test(name);
+    },
+    isValidEmail: function (elementValue) {
+      //got this from http://zparacha.com/validate-email-address-using-javascript-regular-expression
+      var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+      return emailPattern.test(elementValue);
+    },
+  },
+};
+//on submit chekc if name, email, birthdate are valid -
+//if they all return true then and post do the post
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
